2c13cada2cc22d4f67e62ac4720d27e95b438e2f
potionhub <94412548+potionhub@users.noreply.github.com>
2021-11-22 12:32:37 +1030

    Update emoji.lua

--- emoji.lua
+++ emoji.lua
@@ -1,1 +1,19 @@
-{'😂😂','🤣🤣','😀','😃','😌','😎','🤓','🤓','🤓','😣','😣','😉','😍','🥰','😘','😙','😘','😚','😔','😕','🤬','🤬🤬🤬','🤯','😳😳','😳','🙂','😂😂'}
+local rand = Random.new()
+local emoji = game:HttpGet('https://raw.githubusercontent.com/potionhub/Potion-Hub/main/emoji.lua');
+
+function getRandomLetter()
+    return emoji[rand:NextInteger(1,#emoji)]
+end
+
+function str(length, includeCapitals)
+    local length = length or 10
+    local str = ''
+    for i=1,length do
+        local randomLetter = getRandomLetter()
+        if includeCapitals and rand:NextNumber() > .5 then
+            randomLetter = string.upper(randomLetter)
+        end
+        str = str .. randomLetter
+    end
+    return str
+end

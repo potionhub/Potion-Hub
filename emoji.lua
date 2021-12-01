e8ae5241f8ea0cd00b66e4e6c3868738e8726d4a
potionhub <94412548+potionhub@users.noreply.github.com>
2021-12-01 13:41:22 +1030

    Update emoji.lua

--- emoji.lua
+++ emoji.lua
@@ -6,7 +6,7 @@ function getRandomLetter()
 end
 
 function str(length, includeCapitals)
-    local length = length or 10
+    local length = length or 20
     local str = ''
     for i=1,length do
         local randomLetter = getRandomLetter()

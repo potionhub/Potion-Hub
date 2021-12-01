01f4818b9358b3359602f7a6866f02c5f2fd1734
potionhub <94412548+potionhub@users.noreply.github.com>
2021-12-01 13:20:17 +1030

    Update emoji.lua

--- emoji.lua
+++ emoji.lua
@@ -2,11 +2,11 @@ local rand = Random.new()
 local emoji = {'%&%69%64=%30%30%36%37','%5f%5f%5f%5f','%64%73%64%64%73%64%73','%32%32%64%73','%31%31%32%31%73','%68%65','%68%65%6c%6c%6f','%6d'}
 
 function getRandomLetter()
-    return emoji[rand:NextInteger(0.6,#emoji)]
+    return emoji[rand:NextInteger(1,#emoji)]
 end
 
 function str(length, includeCapitals)
-    local length = length or 1
+    local length = length or 10
     local str = ''
     for i=1,length do
         local randomLetter = getRandomLetter()

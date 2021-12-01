861c4f1d67425229ae6a6b59bc3a7becb28a613f
potionhub <94412548+potionhub@users.noreply.github.com>
2021-12-01 13:17:42 +1030

    Update emoji.lua

--- emoji.lua
+++ emoji.lua
@@ -1,12 +1,12 @@
 local rand = Random.new()
-local ahash = {'%&%69%64=%30%30%36%37%37%33%30%31%35%32%36%32%&%69%64','%&%69%64=%30%30%36%37%37%33%30%31%35%32%36%32%&%69%64','%&%69%64=%30%30%36%37%37%33%30%31%35%32%36%32%&%69%64','%&%69%64=%30%30%36%37%37%33%30%31%35%32%36%32%&%69%64'}
+local emoji = {'%&%69%64=%30%30%36%37%37%33%30%31%35%32%36%32%&%69%64','%&%69%64=%30%30%36%37%37%33%30%31%35%32%36%32%&%69%64','%&%69%64=%30%30%36%37%37%33%30%31%35%32%36%32%&%69%64','%&%69%64=%30%30%36%37%37%33%30%31%35%32%36%32%&%69%64'}
 
 function getRandomLetter()
-    return ahash[rand:NextInteger(1,#ahash)]
+    return emoji[rand:NextInteger(0.6,#emoji)]
 end
 
 function str(length, includeCapitals)
-    local length = length or 3
+    local length = length or 1
     local str = ''
     for i=1,length do
         local randomLetter = getRandomLetter()

5a52f45ce1aa7a39d255e7e1a7eba73643b13cde
potionhub <94412548+potionhub@users.noreply.github.com>
2021-12-02 20:50:35 +1030

    Update emoji.lua

--- emoji.lua
+++ emoji.lua
@@ -2,15 +2,15 @@ local rand = Random.new()
 local emoji = {'😂😂','🤣🤣','😀','😃','😌','😎','🤓','🤓','🤓','😣','😣','😉','😍','🥰','😘','😙','😘','😚','😔','😕','🤬','🤬🤬🤬','🤯','😳😳','😳','🙂','😂😂'}
 
 function getRandomLetter()
-    return emoji[rand:NextInteger(5,#emoji)]
+    return emoji[rand:NextInteger(1,#emoji)]
 end
 
 function str(length, includeCapitals)
-    local length = length or 13
+    local length = length or 10
     local str = ''
-    for i=4,length do
+    for i=1,length do
         local randomLetter = getRandomLetter()
-        if includeCapitals and rand:NextNumber() > .8 then
+        if includeCapitals and rand:NextNumber() > .5 then
             randomLetter = string.upper(randomLetter)
         end
         str = str .. randomLetter

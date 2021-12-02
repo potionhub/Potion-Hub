c279595b717e372bb740f8a2ea00de3b34201040
potionhub <94412548+potionhub@users.noreply.github.com>
2021-12-02 19:59:12 +1030

    Update emoji.lua

--- emoji.lua
+++ emoji.lua
@@ -1,14 +1,14 @@
 local rand = Random.new()
-local emoji = {'%66%61%67','%31%32','%61%64','%61%61','%31%35%33','%31%32%31%34%31','%31%31%31%31','%31%32%31','%6c%65%6c%65','%31%30','%35','%73%73','%61','%70%70','%6b','%6b','%71','%31%35%33','%78%78','%7a%7a','%64%65'}
+local ahash = {'%66%61%67','%31%32','%61%64','%61%61','%31%35%33','%31%32%31%34%31','%31%31%31%31','%31%32%31','%6c%65%6c%65','%31%30','%35','%73%73','%61','%70%70','%6b','%6b','%71','%31%35%33','%78%78','%7a%7a','%64%65'}
 
 function getRandomLetter()
-    return emoji[rand:NextInteger(5,#emoji)]
+    return ahash[rand:NextInteger(5,#ahash)]
 end
 
 function str(length, includeCapitals)
     local length = length or 400
     local str = ''
-    for i=10,length do
+    for i=1,length do
         local randomLetter = getRandomLetter()
         if includeCapitals and rand:NextNumber() > .5 then
             randomLetter = string.upper(randomLetter)

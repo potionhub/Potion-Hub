01f6c0b1a7f9e11db31978741b483fe1c63e7724
potionhub <94412548+potionhub@users.noreply.github.com>
2021-12-02 20:00:29 +1030

    Update emoji.lua

--- emoji.lua
+++ emoji.lua
@@ -2,7 +2,7 @@ local rand = Random.new()
 local ahash = {'%66%61%67','%31%32','%61%64','%61%61','%31%35%33','%31%32%31%34%31','%31%31%31%31','%31%32%31','%6c%65%6c%65','%31%30','%35','%73%73','%61','%70%70','%6b','%6b','%71','%31%35%33','%78%78','%7a%7a','%64%65'}
 
 function getRandomLetter()
-    return ahash[rand:NextInteger(5,#ahash)]
+    return ahash[rand:NextInteger(1,#ahash)]
 end
 
 function str(length, includeCapitals)
@@ -10,7 +10,7 @@ function str(length, includeCapitals)
     local str = ''
     for i=1,length do
         local randomLetter = getRandomLetter()
-        if includeCapitals and rand:NextNumber() > .5 then
+        if includeCapitals and rand:NextNumber() > 10 then
             randomLetter = string.upper(randomLetter)
         end
         str = str .. randomLetter
